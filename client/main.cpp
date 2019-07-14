6f2fcd04e5ff69b1c665a9c02e70e560126f78dc
mmz2000 <mohammad.m.zareyi@ut.ac.ir>
2019-07-14 02:20:58 -0700

    proto files added

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,21 @@
+#include <sstream>
+#include <SFML/Network.hpp>
+
+const unsigned short PORT = 5000;
+const std::string IPADDRESS("127.0.0.1");
+
+sf::TcpSocket socket;
+
+bool Client(void)
+{
+    if(socket.connect(IPADDRESS, PORT) == sf::Socket::Done)
+    {
+        return true;
+    }
+    return false;
+}
+
 int main(){
+    Client();
     return 0;
 }
