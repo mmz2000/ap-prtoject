a6ab2cc5312c1f1624731d90bc6e7ed2d1f61ce4
mmz2000 <mohammad.m.zareyi@ut.ac.ir>
2019-07-18 23:40:39 -0700

    register and login completed

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,8 @@
 #include <sstream>
+#include <iostream>
 #include <SFML/Network.hpp>
+#include "request.pb.h"
+#include "communication.pb.h"
 
 const unsigned short PORT = 5000;
 const std::string IPADDRESS("127.0.0.1");
@@ -10,12 +13,124 @@ bool Client(void)
 {
     if(socket.connect(IPADDRESS, PORT) == sf::Socket::Done)
     {
+        std::cout << "connected\n";
         return true;
     }
     return false;
 }
 
+void login()
+{
+    
+    std::stringstream stream;
+    sf::Packet packetSend;
+    sf::Packet packetReceive;
+    Request log;
+    bool done=false;
+    request::Login *temp(new request::Login);
+    std::string username;
+    std::string password;
+    std::string resStr;
+    while(!done)
+    {
+        std::cout<<"enter username\n";
+        std::cin>>username;
+        temp->set_username(username);
+        
+        std::cout<<"enter password\n";
+        std::cin>>password;
+        temp->set_password(password);
+        
+        log.set_allocated_login(temp);
+        
+        log.SerializeToOstream(&stream);
+        packetSend << stream.str();
+        socket.send(packetSend);
+        socket.receive(packetReceive);
+        if (packetReceive >> resStr)
+        {
+            Response rep;
+            rep.ParseFromString(resStr);
+            if(rep.has_login())
+            {
+                if(rep.login().session_id()!=0)
+                    done=true;
+            }
+        }
+    }
+
+}
+
+void reg()
+{
+    std::stringstream stream;
+    sf::Packet packetSend;
+    
+    Request reg;
+    request::Register *temp(new request::Register);
+    std::string name;
+    std::string username;
+    std::string password;
+    std::string confirm;
+    std::string resSrt;
+    bool match = false, done = false;
+    sf::Packet packetReceive;
+    while(!done)
+    {
+        std::cout<<"enter name\n"<<std::flush;
+        std::cin>>name;
+        temp->set_name(name);
+        
+        std::cout<<"enter username\n"<<std::flush;
+        std::cin>>username;
+        temp->set_username(username);
+        
+        
+        std::cout<<"enter password\n"<<std::flush;
+        std::cin>>password;
+        temp->set_password(password);
+        
+        std::cout<<"re-enter password\n"<<std::flush;
+        std::cin>>confirm;
+        temp->set_confirm_password(confirm);
+        reg.set_allocated_register_(temp);
+    
+        reg.SerializeToOstream(&stream);
+        packetSend << stream.str();
+        socket.send(packetSend);
+        socket.receive(packetReceive);
+        if (packetReceive >> resSrt)
+        {
+            Response rep;
+            rep.ParseFromString(resSrt);
+            if(rep.has_register_())
+            {
+                if(rep.register_().success())
+                    done=true;
+            }
+        }
+
+    }
+    //std::cout<<"now try loging in";
+   // login();
+}
+
+void login_register()
+{
+    std::cout << "(L) for login (R) for register";
+    char action;
+    std::cin >> action;
+    if( action == 'L' )
+        login();
+    else if ( action == 'R' )
+        reg();
+    else 
+        std::cout << "wrong input \n goodbye";
+    exit(1);    
+}
+
 int main(){
     Client();
+    login_register();
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,118 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/random_generator.hpp>
+
+#include <map>
+#include <utility>
+
+#include <SFML/Network.hpp>
+
+#include "communication.pb.h"
+#include "database.pb.h"
+
+const unsigned short PORT = 5000;
+std::map<std::string,database::User> users;
+
+sf::TcpListener listener;
+
+int random_id ()
+{
+    boost::uuids::random_generator generator;
+    boost::uuids::uuid uuid1= generator();
+    int a = uuid1.data[0]+uuid1.data[1]+uuid1.data[2]+uuid1.data[3];
+    return a;
+}
+
+
+void openfile ()
+{
+    std::ifstream db("database.db",std::ios::in);
+    std::string str,username;
+    while(db>>str)
+    {
+        database::User user;
+        user.ParseFromString(str);
+        username=user.username();
+        users.insert(std::pair<std::string, database::User>(username,user));
+    }
+    db.close();
+}
+
+response::Login * log(const request::Login Log)
+{
+    response::Login *res(new response::Login);
+    
+    if(users.find(Log.username()) != users.end() && Log.password() == users[Log.username()].password())
+    {
+        res->set_session_id(random_id());
+    } else {
+        res->set_session_id(0);
+    }
+    return res;
+}
+
+response::Register * reg(const request::Register Reg)
+{
+    std::stringstream stream;
+    std::ofstream db("database.db", std::ios::out | std::ios::app);
+    response::Register *res(new response::Register);
+    database::User user;
+    if(users.find(Reg.username()) == users.end() && Reg.password() == Reg.confirm_password())
+    {
+        user.set_id(random_id());
+        user.set_name(Reg.name());
+        user.set_password(Reg.password());
+        user.set_username(Reg.username());
+        users[user.username()]=user;
+
+        user.SerializeToOstream(&stream);        
+        db<<stream.str()<<std::endl;
+        
+        res->set_success(true);
+    } else {
+        res->set_success(false);
+    }
+    db.close();
+    return res;
+}
+
+void Server(void)
+{
+    sf::TcpSocket socket;
+    listener.accept(socket);
+    std::cout<<"connected\n";
+    std::string reqStr;
+    sf::Packet packetReceive;
+    Response res;
+    sf::Packet packetSend;
+    socket.receive(packetReceive);
+    if (packetReceive >> reqStr)
+    {
+        Request req;
+        req.ParseFromString(reqStr);
+        if (req.has_login())
+        {
+            res.set_allocated_login(log(req.login()));
+        } else if(req.has_register_())
+        {
+            res.set_allocated_register_(reg(req.register_()));
+        }
+    }
+    std::stringstream stream;
+    res.SerializeToOstream(&stream);
+    packetSend<<stream.str();
+    socket.send(packetSend);
+}
+
 int main(){
+    openfile();
+
+    listener.listen(PORT);
+    Server();
     return 0;
 }
