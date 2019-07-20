97daeb342718a9a917515816cd27672cbaf59090
mmz2000 <mohammad.m.zareyi@ut.ac.ir>
2019-07-20 15:03:02 -0700

    game running outline

--- server/main.cpp
+++ server/main.cpp
@@ -24,10 +24,138 @@ std::map<int,sf::Thread *> threads;
 std::map<int,bool> EOT;
 std::map<int,database::Queue> Queues;
 std::map<int,int> id2qid;
+std::map<int,database::Game> games;
 
 sf::TcpListener listener;
 sf::Mutex globalMutex;
 
+
+int gid=-1;
+bool new_game=false;
+std::map<int,bool> EOG;
+void game_manager()
+{
+    bool quit=false;
+    int i=0;
+    std::map<int,sf::Thread *> games_running;
+    while(!quit)
+    {
+        globalMutex.lock();
+        if(new_game)
+        {
+            sf::Thread *thread_ptr(new sf::Thread(game_runner,gid));
+            games_running[i] = thread_ptr;
+            thread_ptr->launch();
+            EOG[i]=false;
+        }
+        for(std::map<int,bool>::iterator itr=EOG.begin();itr!=EOG.end();itr++)
+        {
+            if(itr->second)
+            {
+                delete games_running[itr->first];
+                games_running.erase(itr->first);
+                EOG.erase(itr->first);
+            }
+        }
+        globalMutex.unlock();
+    }
+}
+void game_runner(int id)
+{
+    int ** ground;
+    ground=new int *[30];
+    std::map<int,types::PlayerBlock> id2head;
+    int alive_players=0;
+    for (int i = 0; i < 30; i++)
+    {
+        ground[i]= new int [30];
+        for (int j = 0; j < 30; i++)
+            ground[i][j]=-1;
+    }
+    for (database::Player PLAYER:games[id].players())
+    {
+        ground[PLAYER.block_info().head().position().x()][PLAYER.block_info().head().position().y()]=PLAYER.user_id();
+        id2head[PLAYER.user_id()]=PLAYER.block_info().head();
+        for (types::PlayerBlock PB:PLAYER.block_info().blocks())
+        {
+            ground[PB.position().x()][PB.position().x()]=PLAYER.user_id();
+        }
+        alive_players++;
+    }
+    for (types::FoodBlock FB:games[id].foods())
+    {
+        ground[FB.position().x()][FB.position().x()]=FB.benefit();
+    }
+    for (types::WallBlock WB:games[id].walls())
+    {
+        ground[WB.position().x()][WB.position().x()]=-2;
+    }
+    while(alive_players>1)
+    {
+/*game logic goes here */
+    }
+    EOG[id]=true;
+}
+void game_maker(const database::Queue Q)
+{
+    database::Game GAME;
+    GAME.set_id(Q.id());
+    for(int a:Q.user_ids())
+    {
+        database::Player * PLAYER = GAME.add_players();
+        PLAYER->set_user_id(a);
+        types::Player * player(new types::Player);
+        player->set_name(users[id2user[a]].name());
+        player->set_is_alive(true);
+        types::PlayerBlock * PB(new types::PlayerBlock);
+        types::Block * B (new types::Block);
+        B->set_x(random_id()%28+1);
+        B->set_y(random_id()%28+1);
+        PB->set_allocated_position(B);
+        player->set_allocated_head(PB);
+        PLAYER->set_allocated_block_info(player);
+    }
+    for(int i=1;i<30;i++)
+    {
+        types::WallBlock * WB1 =GAME.add_walls();
+        types::Block * B1 (new types::Block);
+        B1->set_x(0);
+        B1->set_y(i);
+        WB1->set_allocated_position(B1);
+        
+        types::WallBlock * WB2 =GAME.add_walls();
+        types::Block * B2 (new types::Block);
+        B2->set_x(29);
+        B2->set_y(i);
+        WB2->set_allocated_position(B2);
+        
+        types::WallBlock * WB3 =GAME.add_walls();
+        types::Block * B3 (new types::Block);
+        B3->set_x(i);
+        B3->set_y(0);
+        WB3->set_allocated_position(B3);
+        
+        types::WallBlock * WB4 =GAME.add_walls();
+        types::Block * B4 (new types::Block);
+        B4->set_x(i);
+        B4->set_y(29);
+        WB4->set_allocated_position(B4);
+    }
+    for(int i=0;i<5;i++)
+    {
+        types::FoodBlock * FB =GAME.add_foods();
+        types::Block * B (new types::Block);
+        B->set_x(random_id()%28+1);
+        B->set_y(random_id()%28+1);
+        FB->set_allocated_position(B);
+        FB->set_benefit(random_id()%4+1);
+    }
+    games[Q.id()]=GAME;
+    globalMutex.lock();
+    gid=Q.id();
+    new_game=true;
+    globalMutex.unlock();
+}
 
 int random_id ()
 {
@@ -211,7 +339,13 @@ response::QueueStatus * Q_status(const request::QueueStatus QS)
     }
     r_value->set_size(Queues[Q_id].size());
     if(Queues[Q_id].user_ids_size()==Queues[Q_id].size())
+    {
         r_value->set_initialized(true);
+        if(games.find(Q_id)==games.end())
+        {
+            game_maker(Queues[Q_id]);
+        }
+    }
     else
         r_value->set_initialized(false);
     return r_value;
